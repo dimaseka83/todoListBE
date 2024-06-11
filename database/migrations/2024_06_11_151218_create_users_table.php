202e804d6333c0a1131408adc1384156be3f8a9f
eka <dimaseka83@gmail.com>
2024-06-12 01:48:40 +0700

    ++

--- database/migrations/2024_06_11_151218_create_users_table.php
+++ database/migrations/2024_06_11_151218_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
         });
