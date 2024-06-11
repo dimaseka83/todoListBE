b5c8b7ed36061dd44f28c4eea2b36ab58a3fbfad
eka <dimaseka83@gmail.com>
2024-06-12 00:16:21 +0700

    add pdo

---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'lumen'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'timezone' => '+07:00',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+    ],
+
+];
